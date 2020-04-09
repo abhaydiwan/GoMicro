e12bb08b2dd00304624c0aa0995afb60db650895
Abhay Diwan <abhaykumar.diwan@fujitsu.com>
2020-04-08 20:37:16 -0400

    fixed

---
+++ server/go.mod
@@ -0,0 +1,8 @@
+module testapp
+
+go 1.14
+
+require (
+	github.com/abhaydiwan/GoMicro v0.0.0-20200409001505-118628888cc0
+	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e
+)
--- server/main.go
+++ server/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"time"
 
-	proto "github.com/PacktPublishing/Advanced-Cloud-Native-Go/Communication/Go-Micro/proto"
+	proto "github.com/abhaydiwan/GoMicro/proto"
 	micro "github.com/micro/go-micro"
 	"golang.org/x/net/context"
 )
